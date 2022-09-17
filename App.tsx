12bc9432ae1281634e8d7b241fbc6abb908e57be
Ricardo Hitalo Araujo Soares <ricardohitalo1@gmail.com>
2022-09-17 12:35:12 -0300

    feature-> Home page application

--- App.tsx
+++ App.tsx
@@ -3,18 +3,20 @@ import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
-  Inter_900Black
+  Inter_700Bold,
+  Inter_900Black,
 } from '@expo-google-fonts/inter'
 
 import { Background } from './src/components/Background';
 import { Home } from './src/screens/Home';
-import { Loading } from './src/screens/Loading';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
-    Inter_900Black
+    Inter_700Bold,
+    Inter_900Black,
   });
 
   return (
